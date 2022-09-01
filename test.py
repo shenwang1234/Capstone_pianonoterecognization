b5206367ae2b3b80efc13741864f360aae3d5956
fiona <fiona@fionas-MacBook-Air.local>
2022-08-31 22:17:42 -0400

    add mode

--- test.py
+++ test.py
@@ -2,17 +2,17 @@ import os
 
 import numpy.fft
 from scipy.io import wavfile
-import scipy.io
-from pydub import AudioSegment
+import sounddevice as sd
 import matplotlib.pyplot as plt
 import numpy as np
 from os.path import join as pjoin
 from scipy import signal
 from scipy import fft
 import math
+import time
 
 
-note_string = ['C', 'C#/Db' , 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B']
+note_string = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B']
 
 
 def path_to_numpy(file_path):
@@ -60,30 +60,67 @@ def slice_and_find_note(audio_signal, frame_rate, section_length, threshold):
     total_duration = len(audio_signal) / frame_rate
     n_section = math.ceil(total_duration / (section_length / 1000))  # number of sections in the audio file. secionts split using section len
     sample_per_section = int((section_length / 1000) * frame_rate)
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
     for i in range(0, n_section):  # calculate each section and plot it
         section = audio_signal[i * sample_per_section: min((i + 1) * sample_per_section, len(filtered_signal))]  # chop into section
         freq, signal_amp = generate_freq_spectrum(section, frame_rate)  # fft
         peak_freq_index = numpy.argmax(signal_amp)  # get peak freq, return the index of the highest value
         note_name = frequency_to_note(freq[peak_freq_index])
         if signal_amp[peak_freq_index] > threshold:
-            plt.text(i * sample_per_section, 0, note_name)  # plot note name
-        plt.axvline(x=min((i + 1) * sample_per_section, len(signal_numpy)), color='r', linewidth=0.5, linestyle="-",zorder=10)  # lines for separating segments
-        plt.text(i * sample_per_section, 2000, round(signal_amp[peak_freq_index]))  # plot the freq magnitude
+            ax.text(i * sample_per_section, 0, note_name)  # plot note name
+        ax.axvline(x=min((i + 1) * sample_per_section, len(audio_signal)), color='r', linewidth=0.5, linestyle="-",zorder=10)  # lines for separating segments
+        #change 2000 to scal
+
+        ax.text(i * sample_per_section, 2000, round(signal_amp[peak_freq_index]))  # plot the freq magnitude
+    y_scale = ax.get_ylim()
+    print(y_scale)
     plt.plot(audio_signal, zorder=0)
 
 def to_do():
-    #clean up
-    #interface mac mic
+    #record
+    #real time
     # research terminalgy
     pass
 
 
 if __name__ == "__main__":
-    path = "Capstone piano test 1.wav"
-    frame_rate, signal_numpy = path_to_numpy(path)
-    filtered_signal = bandpass_filter(signal_numpy)
-    section_len = 500  # length of each section in ms
-    magnitude_threshold = 50
-    slice_and_find_note(filtered_signal, frame_rate, section_len, magnitude_threshold)
-    plt.show()
+    print("enter mode number: 1 for real, 2 for record, 3 for file")
+    mode = int(input())
+    if mode == 3:
+        path = "Capstone piano test 1.wav"
+        frame_rate, signal_numpy = path_to_numpy(path) #signal_numpy shape (n,)
+        filtered_signal = bandpass_filter(signal_numpy)
+        section_len = 500  # length of each section in ms
+        magnitude_threshold = 50
+        slice_and_find_note(filtered_signal, frame_rate, section_len, magnitude_threshold)
+        plt.show()
+    if mode == 2:
+        fs = 48000
+        duration = 10
+        recording_signal = sd.rec(int(duration * fs), samplerate=fs, channels=1) #return type numpy.ndarry shape (n,1)
+        recording_signal = recording_signal.reshape((len(recording_signal),))
+        sd.wait()
+        filtered_signal = bandpass_filter(recording_signal)
+        section_len = 500  # length of each section in ms
+        magnitude_threshold = 0 # need to change this to value based on average. find normal not average
+        # root mean square, rms (x-mean)
+        slice_and_find_note(filtered_signal, fs, section_len, magnitude_threshold)
+        plt.show()
+    if mode == 1:  # note mapping is wrong
+        time = 10
+        fs = 48000
+        current = 0
+        while current < time*fs:
+            recording_signal = sd.rec(int(0.5 * fs), samplerate=fs, channels=1)
+            sd.wait()
+            freq, signal_amp = generate_freq_spectrum(recording_signal, fs)  # fft
+            peak_freq_index = numpy.argmax(signal_amp)  # get peak freq, return the index of the highest value
+            note_name = frequency_to_note(freq[peak_freq_index])
+            print(note_name)
+            current += fs
 
+
+
+
+#1.5
